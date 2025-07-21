0656db093f4b0a6a7df95ce0fdc0cba6eba0ab7b
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-21 10:41:23 +0530

    Correct cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(cors({origin: "https://real-estate-jade-alpha.vercel.app"}));
+app.use(
+  cors({
+    origin: "https://real-estate-jade-alpha.vercel.app",
+  })
+);
 app.use(express.json());
 
 app.use("/api/user", UserRouter);
