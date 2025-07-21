795bdb9490d62a58170876ace7712321a03434dc
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-21 09:22:27 +0530

    Initial backend commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({origin: "https://real-estate-jade-alpha.vercel.app"}));
 app.use(express.json());
 
 app.use("/api/user", UserRouter);
